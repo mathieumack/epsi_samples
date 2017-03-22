14233cfb03e6a31ee4fd1f8aa916fceec7bb698c
MACK Mathieu <mathieu.mack@live.fr>
2017-03-22 21:47:33 +0100

    Cours 6 Fizz Buzz sample

---
+++ csharp/b2/cours_6/Cours_6/Cours_6/FizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿namespace Cours_6
+{
+    public class FizzBuzz
+    {
+        public string KataFb(int n)
+        {
+            string result = string.Empty;
+            for(int i = 1; i <= n; i++)
+            {
+                bool check = false;
+                result += GetMultiple5et3(i, out check);
+                if(!check)
+                    result += GetMultiple5(i, out check);
+                if (!check)
+                    result += GetMultiple3(i, out check);
+                if (!check)
+                    result += i.ToString();
+            }
+            return result;
+        }
+
+        private string GetMultiple5et3(int n, out bool isOk)
+        {
+            isOk = n % 5 == 0 && n%3 == 0;
+            if (isOk)
+                return "FizzBuzz";
+            return string.Empty;
+        }
+
+        private string GetMultiple3(int n, out bool isOk)
+        {
+            isOk = n % 3 == 0;
+            if (isOk)
+                return "Fizz";
+            return string.Empty;
+        }
+
+        private string GetMultiple5(int n, out bool isOk)
+        {
+            isOk = n % 5 == 0;
+            if (isOk)
+                return "Buzz";
+            return string.Empty;
+        }
+    }
+}
---
+++ csharp/b2/cours_6/Cours_6/Cours_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cours_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 0 and 100 : (Validate by enter)");
+            int value = int.Parse(Console.ReadLine());
+
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            Console.WriteLine("Result :");
+
+            Console.WriteLine(fizzBuzz.KataFb(value));
+
+            Console.ReadLine();
+        }
+    }
+}
