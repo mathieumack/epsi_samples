b3768d8010fdc2f23d997c6ad8a35a81883d3e85
MACK Mathieu <mathieu.mack@live.fr>
2017-04-06 08:18:43 +0200

    First version of DM

--- csharp/b2/cours_7/Cours_7/Cours_7.Unit.Tests/BibliothequeTests.cs
+++ csharp/b2/cours_7/Cours_7/Cours_7.Unit.Tests/BibliothequeTests.cs
@@ -14,65 +14,87 @@ namespace Cours_7.Unit.Tests
         {
             bibliotheque = new Bibliotheque();
 
-            List<Livre> livres = new List<Livre>();
-            for (int i = 1; i < 10; i++)
-            {
-                livres.Add(new Livre()
-                {
-                    Id = i,
-                    Titre = "Titre 1",
-                    IsReservable = i < 8,
-                    DateDisponibilite = null
-                });
-            }
-
-            List<Utilisateur> utilisateurs = new List<Utilisateur>();
-            for (int i = 1; i < 10; i++)
-            {
-                utilisateurs.Add(new Utilisateur()
-                {
-                    Id = i,
-                    Nom = "Monsieur " + i.ToString()
-                });
-            }
-
-            bibliotheque.InitDatas(livres, utilisateurs);
+            // On initialize avec des données fake :
+            bibliotheque.InitDatas(GetFakeLivres(), GetFakeUtilisateurs());
         }
 
         [TestMethod]
         public void IsReservable()
         {
-            bool isReservable = bibliotheque.IsReservable(1);
-            Assert.IsTrue(isReservable);
-
-            isReservable = bibliotheque.IsReservable(2);
-            Assert.IsFalse(isReservable);
+            Assert.IsTrue(bibliotheque.IsReservable(0));
+            Assert.IsFalse(bibliotheque.IsReservable(1));
+            Assert.IsTrue(bibliotheque.IsReservable(6));
+            Assert.IsFalse(bibliotheque.IsReservable(9));
+            Assert.IsFalse(bibliotheque.IsReservable(12));
         }
 
         [TestMethod]
         public void ReserveLivre_meme_livre_meme_date()
         {
-            bool canReserver = bibliotheque.ReserveLivre(2, 1, new DateTime(2017, 2, 1));
+            bool canReserver = bibliotheque.ReserveLivre(2, 1, DateTime.Now);
             Assert.IsTrue(canReserver);
 
-            canReserver = bibliotheque.ReserveLivre(2, 1, new DateTime(2017, 2, 1));
+            // Même livre à la même date pour le même utilisateur :
+            canReserver = bibliotheque.ReserveLivre(2, 1, DateTime.Now);
+            Assert.IsFalse(canReserver);
+
+            // Même livre à la même date pour un autre utilisateur :
+            canReserver = bibliotheque.ReserveLivre(2, 3, DateTime.Now);
             Assert.IsFalse(canReserver);
         }
 
         [TestMethod]
         public void ReserveLivre_3fois_par_utilisateur()
         {
-            bool canReserver = bibliotheque.ReserveLivre(1, 1, new DateTime(2017, 2, 1));
+            bool canReserver = bibliotheque.ReserveLivre(2, 1, DateTime.Now.AddDays(10));
             Assert.IsTrue(canReserver);
 
-            canReserver = bibliotheque.ReserveLivre(2, 1, new DateTime(2017, 2, 1));
+            canReserver = bibliotheque.ReserveLivre(4, 1, DateTime.Now.AddDays(10));
             Assert.IsTrue(canReserver);
 
-            canReserver = bibliotheque.ReserveLivre(3, 1, new DateTime(2017, 2, 1));
+            canReserver = bibliotheque.ReserveLivre(6, 1, DateTime.Now.AddDays(10));
             Assert.IsTrue(canReserver);
 
-            canReserver = bibliotheque.ReserveLivre(4, 1, new DateTime(2017, 2, 1));
+            canReserver = bibliotheque.ReserveLivre(8, 1, DateTime.Now.AddDays(10));
             Assert.IsFalse(canReserver);
         }
+
+        #region Fake datas
+
+        private List<Livre> GetFakeLivres()
+        {
+            List<Livre> result = new List<Livre>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                result.Add(new Livre()
+                {
+                    Id = i,
+                    Titre = "Livre " + i.ToString(),
+                    IsReservable = i % 2 == 0, // Seuls les nombres pairs sont réservables :
+                    DateDisponibilite = DateTime.Now.AddDays(i-3) // Tous les jours à partir d'aujourd'hui - 3 jours un nouveau livre est réservable
+                });
+            }
+
+            return result;
+        }
+
+        private List<Utilisateur> GetFakeUtilisateurs()
+        {
+            List<Utilisateur> result = new List<Utilisateur>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                result.Add(new Utilisateur()
+                {
+                    Id = i,
+                    Nom = "Nom " + i.ToString()
+                });
+            }
+
+            return result;
+        }
+
+        #endregion
     }
 }
--- csharp/b2/cours_7/Cours_7/Cours_7/Bibliotheque.cs
+++ csharp/b2/cours_7/Cours_7/Cours_7/Bibliotheque.cs
@@ -6,11 +6,14 @@ namespace Cours_7
 {
     public class Bibliotheque : IBibliotheque
     {
-        private IList<Livre> Livres { get; set; }
+        private int nbJoursResaParLivre = 2;
+        private int nbResaParUtilisateursMax = 3;
 
-        private IList<Emprunt> Emprunts { get; set; }
+        private List<Livre> Livres { get; set; }
 
-        private IList<Utilisateur> Utilisateurs { get; set; }
+        private List<Emprunt> Emprunts { get; set; }
+
+        private List<Utilisateur> Utilisateurs { get; set; }
 
         #region Constructor
 
@@ -22,13 +25,12 @@ namespace Cours_7
         #endregion
 
         #region Public methods
-
-
+        
 
         #endregion
 
         #region Initialisation de la bibliotheque
-
+        
         public void InitDatas()
         {
             Emprunts = new List<Emprunt>();
@@ -38,6 +40,11 @@ namespace Cours_7
 
         public void InitDatas(List<Livre> livres, List<Utilisateur> utilisateurs)
         {
+            if (livres == null)
+                throw new ArgumentNullException();
+            if (utilisateurs == null)
+                throw new ArgumentNullException();
+
             Emprunts = new List<Emprunt>();
             InitLivres(livres);
             InitUtilisateurs(utilisateurs);
@@ -45,14 +52,26 @@ namespace Cours_7
 
         private void InitLivres(List<Livre> livres)
         {
+            if (livres == null)
+                throw new ArgumentNullException();
+
             Livres = livres;
         }
 
         private void InitUtilisateurs(List<Utilisateur> utilisateurs)
         {
+            if (utilisateurs == null)
+                throw new ArgumentNullException();
+
             Utilisateurs = utilisateurs;
         }
 
+        /// <summary>
+        /// Indique si un livre est réservable ou pas.
+        /// Un livre est réservable s'il existe dans la bibliothèque et qu'il est flagué comme réservable
+        /// </summary>
+        /// <param name="livreId"></param>
+        /// <returns></returns>
         public bool IsReservable(int livreId)
         {
             if (Livres.Any(e => e.Id == livreId))
@@ -61,8 +80,65 @@ namespace Cours_7
                 return false;
         }
 
+        /// <summary>
+        /// Indique si un livre est disponible ou pas à une date donnée
+        /// </summary>
+        /// <param name="livreId"></param>
+        /// <param name="dateReservation"></param>
+        /// <returns></returns>
+        public bool IsDisponible(int livreId, DateTime dateReservation)
+        {
+            return Livres.Any(e => e.Id == livreId && e.DateDisponibilite <= dateReservation);
+        }
+
+        /// <summary>
+        /// Permet de réserver un livre dans la bibliothèque
+        /// </summary>
+        /// <param name="livreId"></param>
+        /// <param name="utilisateurId"></param>
+        /// <param name="dateReservation"></param>
+        /// <returns>Vrai si le livre e été réservé, faux s'il ne peut pas l'être</returns>
         public bool ReserveLivre(int livreId, int utilisateurId, DateTime dateReservation)
         {
+            // Nous vérifions que le livre est réservable, et que l'utilisateur aussi :
+            if (IsReservable(livreId) &&
+                IsDisponible(livreId, dateReservation) &&
+                Utilisateurs.Any(e => e.Id == utilisateurId))
+            {
+                // On génère les 2 dates d'emprunt pour le livre :
+                List<Emprunt> emprunts = new List<Emprunt>();
+                for (int i = 0; i < nbJoursResaParLivre; i++)
+                {
+                    emprunts.Add(new Emprunt()
+                    {
+                        LivreId = livreId,
+                        UtilisateurId = utilisateurId,
+                        DateReservation = dateReservation.Date.AddDays(i)
+                    });
+                }
+
+                // On récupère les dates :
+                List<DateTime> dates = emprunts.Select(e => e.DateReservation).ToList();
+
+                // On vérifie que le livre n'est pas déjà réservé pour toutes les dates ci-dessus :
+                bool dejaEmprunte = Emprunts.Any(e => e.LivreId == livreId && dates.Contains(e.DateReservation.Date));
+
+                if (!dejaEmprunte)
+                {
+                    // On vérifie aussi que l'utilisateur n'a pas déjà d'autres réservations
+                    bool utilisateurPeutReserver = true;
+                    foreach (DateTime date in dates)
+                    {
+                        utilisateurPeutReserver &= Emprunts.Count(e => e.UtilisateurId == utilisateurId && e.DateReservation == date) < nbResaParUtilisateursMax;
+                    }
+
+                    if(utilisateurPeutReserver)
+                        Emprunts.AddRange(emprunts);
+
+                    return utilisateurPeutReserver;
+                }
+            }
+
             return false;
         }
 
